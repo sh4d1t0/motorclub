b06467c95160774a17219427054b098b9e65943e
Unknown <shadito@live.com.mx>
2017-11-28 20:05:41 -0600

    CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,22 +2,150 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
     public function index()
     {
-        return 'Usuarios';
+	    // get all the users
+	    $users = User::all();
+
+	    // load the view and pass the users
+	    return view('users.index')
+		    ->with('users', $users);
+	    /*return View::make('users.index')
+	               ->with('users', $users);*/
     }
 
-    public function show($id)
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		// load the create form (app/views/users/create.blade.php)
+		return view('users.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+    public function store()
     {
-        return "Mostrando detalle del usuario: {$id}";
+	    // validate
+	    // read more on validation at http://laravel.com/docs/validation
+	    $rules = array(
+		    'firstname'  => 'required',
+		    'lastname'   => 'required',
+		    'email'      => 'required|email'
+	    );
+	    $validator = Validator::make(Input::all(), $rules);
+
+	    // process the login
+	    if ($validator->fails()) {
+		    return Redirect::to('nerds/create')
+		                   ->withErrors($validator)
+		                   ->withInput(Input::except('password'));
+	    } else {
+		    // store
+		    $user = new Nerd;
+		    $user->firstname  = Input::get('firstname');
+		    $user->lastname   = Input::get('lastname');
+		    $user->email      = Input::get('email');
+		    $user->save();
+
+		    // redirect
+		    Session::flash('message', 'Successfully created user!');
+		    return Redirect::to('users');
+	    }
     }
 
-    public function create()
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		// get the nerd
+		$user = User::find($id);
+
+		// show the view and pass the nerd to it
+		return view('users.show')
+		           ->with('user', $user);
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+    public function edit($id)
     {
-        return 'Crear nuevo usuario';
+	    // get the user
+	    $user = User::find($id);
+
+	    // show the edit form and pass the user
+	    return view('users.edit')
+	               ->with('user', $user);
+    }
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+    public function update($id)
+    {
+	    // validate
+	    // read more on validation at http://laravel.com/docs/validation
+	    $rules = array(
+		    'firstname'  => 'required',
+		    'lastname'   => 'required',
+		    'email'      => 'required|email'
+	    );
+	    $validator = Validator::make(Input::all(), $rules);
+
+	    // process the login
+	    if ($validator->fails()) {
+		    return Redirect::to('usuarios/' . $id . '/editar')
+		                   ->withErrors($validator)
+		                   ->withInput(Input::except('password'));
+	    } else {
+		    // store
+		    $user = User::find($id);
+		    $user->firstname       = Input::get('firstname');
+		    $user->lastname       = Input::get('lastname');
+		    $user->email      = Input::get('email');
+		    $user->save();
+
+		    // redirect
+		    Session::flash('message', 'Successfully updated user!');
+		    return Redirect::to('users');
+	    }
+    }
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+    public function destroy($id)
+    {
+    	//
     }
 }
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->firstname }} {{ Auth::user()->lastname }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                            @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+@yield('content')
+<!-- #################### -->
+
+    <h1>Editar {{ $user->firstname }}</h1>
+
+    <form method="put" action="{{url('users.update', $user->id)}}">
+        {{csrf_field()}}
+        <div class="form-group">
+            <label for="firstname">Firstname</label>
+            <input type="text" value="{{ $user->firstname }}" class="form-control" id="firstname" name="firstname">
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" value="{{ $user->email }}" class="form-control" id="email" name="email">
+        </div>
+        <button type="submit" class="btn btn-primary">Editar el usuario</button>
+    </form>
+
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
