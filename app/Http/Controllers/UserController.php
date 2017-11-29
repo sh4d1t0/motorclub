e63832802fcf68825a3677d7861544ea0eb00cbc
Unknown <shadito@live.com.mx>
2017-11-29 17:54:31 -0600

    FIX Users Auth Middleware Restriction

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,6 +19,7 @@ class UserController extends AppBaseController
     public function __construct(UserRepository $userRepo)
     {
         $this->userRepository = $userRepo;
+	    $this->middleware('auth');
     }
 
     /**
