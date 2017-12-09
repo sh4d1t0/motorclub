70a9019694238a73ab69223b34778caef3bf1e92
sh4d1t0 <shadito@live.com.mx>
2017-12-09 15:54:44 -0600

    New Documents template
    new web design for documents template

--- resources/views/files/create.blade.php
+++ resources/views/files/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="grid-container fluid">
         @include('adminlte-templates::common.errors')
-        <div class="grid-x grid-margin-x">
+        <div class="grid-x grid-margin-x grid-padding-y">
             <div class="cell hide-for-small-only medium-2 large-4"><!-- ### --></div>
             <div class="cell small-12 medium-8 large-4">
                 <div class="card">
--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -1,36 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class='view_parent_image1'>
-        <div class="grid-container fluid">
-            <div class="grid-x grid-margin-x">
-                <div class="cell hide-for-small-only medium-2 large-4"><!-- ### --></div>
-                <div class="cell small-12 medium-8 large-4">
-                    <div class="card">
-                        @include('flash::message')
-                        <div class="card-divider">
-                            <h1>El club</h1>
+    <div class="grid-container fluid">
+        <div class="grid-x grid-margin-x grid-padding-y">
+            <div class="cell hide-for-small-only medium-2 large-2"><!-- ### --></div>
+            <div class="cell small-12 medium-8 large-8">
+                <h1 class="text-right text-overline">Documentos</h1>
+                <div class="grid-x grid-margin-x grid-padding-y">
+                    @include('flash::message')
+                    @foreach($files as $files)
+                        <div class="cell small-3">
+                            <p>{!! $files->title !!}</p>
+                            <div class='button-group'>
+                                <a href="{{ url('el-club/documentos/'.$files->id.'/ver') }}" class='clear button primary'><i class="fi-eye size-21"></i></a>
+                                <a href="{{ url('el-club/documentos/'.$files->id.'/descargar') }}" class='clear button primary'><i class="fi-page-export-pdf size-21"></i></a>
+                            </div>
                         </div>
-                        <div class="card-section">
-                            <p>Somos un club que esta afiliado al moto club
-                            nacional BMW y legalmente constituidos
-                            siendo uno de los clubes más numerosos y
-                            de suma importancia a nivel nacional.</p>
-                            <div class="clear"></div>
-                            <a class="button primary">
-                                Documentos
-                            </a>
-                            <a class="button primary">
-                                Mesa Directiva
-                            </a>
-                            @include('flash::message')
-                            <div class="clear"></div>
-                            @include('files.table')
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
-                <div class="cell hide-for-small-only medium-2 large-4"><!-- ### --></div>
             </div>
+            <div class="cell hide-for-small-only medium-2 large-2"><!-- ### --></div>
         </div>
     </div>
 @endsection
